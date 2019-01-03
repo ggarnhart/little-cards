8529fc77acbe1ebee02d7e5add0b83cc0f2094bf
Greg Garnhart <gegarnhart@email.wm.edu>
2019-01-03 11:55:08 -0500

    Updates

---
+++ precache-manifest.143e20b13f8e2543b00af082abaebe9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/little-cards/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "974a5776bd793aba4b8f",
+    "url": "/little-cards/static/js/runtime~main.974a5776.js"
+  },
+  {
+    "revision": "a491d1d6e7d988ffa46f",
+    "url": "/little-cards/static/js/main.a491d1d6.chunk.js"
+  },
+  {
+    "revision": "1fb0eaee7048f35a64b2",
+    "url": "/little-cards/static/js/1.1fb0eaee.chunk.js"
+  },
+  {
+    "revision": "a491d1d6e7d988ffa46f",
+    "url": "/little-cards/static/css/main.b4cd70e6.chunk.css"
+  },
+  {
+    "revision": "1fb0eaee7048f35a64b2",
+    "url": "/little-cards/static/css/1.ac946848.chunk.css"
+  },
+  {
+    "revision": "fe9c97ceb9110680550391baa0551a7a",
+    "url": "/little-cards/index.html"
+  }
+];
